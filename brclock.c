b58b755686c596e50038d9dc409b564d0d40ab90
Tudor <xenogenesis@openmailbox.org>
2016-07-15 21:46:32 +0300

    refactor code

--- brclock.c
+++ brclock.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <time.h>
 #include <locale.h>
+#include <wchar.h>
 
 #define LIN 4
 #define COL 2
@@ -11,8 +12,10 @@
 char* toDotArray(char n);
 wchar_t toBraille(char *m[COL]);
 
+/* Holds binary representation of hours, minutes and seconds */
 char *mat[3][COL];
 
+/* Map for braille dots exponents */
 char map[COL][LIN] = {{0, 1, 2, 6},
                       {3, 4, 5, 7}};
 
@@ -32,11 +35,13 @@ int main() {
     mat[2][0] = toDotArray(tm.tm_sec / 10);
     mat[2][1] = toDotArray(tm.tm_sec % 10);
 
+    /* Print wide chars */
     setlocale(LC_ALL, "");
-    printf("%lc\n%lc\n%lc\n", toBraille(mat[0]), toBraille(mat[1]), toBraille(mat[2]));
+    wprintf(L"%C\n%C\n%C\n", toBraille(mat[0]), toBraille(mat[1]), toBraille(mat[2]));
     return 0;
 }
 
+/* Get binary representation of n */
 char* toDotArray(char n) {
     char *ret = malloc(LIN * sizeof(char));
     int i = LIN - 1;
@@ -50,7 +55,9 @@ char* toDotArray(char n) {
     return ret;
 }
 
+/* binary number to braille character */
 wchar_t toBraille(char *m[COL]) {
+    /* START is the empty braille character */
     wchar_t c = START;
     for (int i = 0; i < COL; i++) {
         for (int j = 0; j < LIN; j++) {
